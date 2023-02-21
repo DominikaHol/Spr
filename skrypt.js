5bf1a48fef94a7a351499ea28258a06f6069fd8b
DominikaHol <98097626+DominikaHol@users.noreply.github.com>
2023-02-21 15:46:49 +0100

    Delete skrypt.js

--- skrypt.js
+++
@@ -1,8 +0,0 @@
-function oblicz(){
-    let a1=parseInt(document.getElementById("a1").value);
-    let an=parseInt(document.getElementById("an").value);
-    let n=parseInt(document.getElementById("n").value);
-    let Wynik=(an-a1)/(n-1);
-
-document.getElementById("wynik").value=Wynik;
-}
